b4f49e8b7187ce792781f2a5e73ba8883d0d46f6
Gustavo Paes <gustavo910@gmail.com>
2022-07-20 15:34:06 -0300

    2.2- metodo criar recebendo inputStream e nome texto-ok

--- src/ccom/alura/stickers/App250Filmes.java
+++ src/ccom/alura/stickers/App250Filmes.java
@@ -1,7 +1,10 @@
 package ccom.alura.stickers;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 public class App250Filmes {
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws Exception {
 //	https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html
 //	https://imdb-api.com/en/API/Top250TVs/k_i5x35d3q
 //		String key = "k_i5x35d3q";
@@ -18,16 +21,18 @@ public class App250Filmes {
 
 		
 		// Conexap HTTP:
-		String url = "https://imdb-api.com/en/API/Top250Movies/k_i5x35d3q";
+		String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+//		String url = "https://imdb-api.com/en/API/Top250Movies/k_i5x35d3q";
 		URI endereco = URI.create(url);// endereco
 
 		HttpClient client = HttpClient.newHttpClient();// cliente
 		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();// requisição/request
 
+		// Lista de dados:
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		String body = response.body();
 		System.out.println(body);
-		System.out.println("status da solicitação: " + response.statusCode());
+		System.out.println("status da solicitação: " + response.statusCode()+"\n");
 		
 		//Extrair dados JSON :
 		JsonParser parse = new JsonParser();
@@ -35,13 +40,36 @@ public class App250Filmes {
 		List<Map<String, String>> listaDeFilmes = parse.parse(body);
 		
 		// EXibir dados e Manipular:
+		GeradorDeFigurinhas gdf= new GeradorDeFigurinhas();
 		
-		for(Map<String, String> item : listaDeFilmes) {
-			System.out.println("Titulo: " + item.get("title"));
-			System.out.println("Imagem: " + item.get("image"));
-			System.out.println("Avaliação: " + item.get("imDbRating"));
-			System.out.println();
+		System.out.println("começou!");
+		for(int i=0; i<5; i++) {
+			String urlImage= listaDeFilmes.get(i).get("image");
+			String titleImage="My-"+listaDeFilmes.get(i).get("title")+".jpg";
+			
+			InputStream ips= new URL(urlImage).openStream();
+			
+			
+//			gdf.criar(ips, titleImage);
+			System.out.println("Titulo: "+listaDeFilmes.get(i).get("title"));
 		}
+			
+		
+//		for(Map<String, String> item : listaDeFilmes) {
+//			String urlImage= item.get("image");
+//			String titleImage="My "+item.get("title")+".png";
+//			
+//			InputStream ips= new URL(urlImage).openStream();
+//			
+//			
+//			gdf.criar(ips, titleImage);
+//			gdf.criar(null, body);
+//			System.out.println("Titulo: " + item.get("title"));
+//			System.out.println("Imagem: " + item.get("image"));
+//			System.out.println("Avaliação: " + item.get("imDbRating"));
+//			System.out.println();
+			
+		
 
 		System.out.println("e ai, fez algo de bom?");
 	}
--- src/ccom/alura/stickers/GeradorDeFigurinhas.java
+++ src/ccom/alura/stickers/GeradorDeFigurinhas.java
@@ -1,10 +1,12 @@
 package ccom.alura.stickers;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.awt.Color;
 import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -12,47 +14,54 @@ public class GeradorDeFigurinhas {
 
 	// leitura da imagem
 
-	public void criar() throws Exception {
-		File arquivo = new File("entrada/sda.jpg");
+	public void criar(InputStream inputStream, String nomeArquivo) throws Exception {
+//		https://github.com/alura-cursos/imersao-java/blob/aula2/alura-stickers/src/GeradoraDeFigurinhas.java
 
-//		InputStream ips = new FileInputStream(arquivo);
+//		String url = "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,3,128,176_AL_.jpg\r\n";
+//		InputStream ipsUrl = new URL(url).openStream();
+//		classe que presenta uma fonte de bytes com isso, posso receber e transformar no tipo de arquivo desejado
 
-		BufferedImage imagemOriginal = ImageIO.read(arquivo);
+//		File arquivo = new File("entrada/sda.jpg");
+
+//		InputStream ips2= new FileInputStream(new File("entrada/sda.jpg"));
+
+		BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
 		int largura = imagemOriginal.getWidth();
 		int altura = imagemOriginal.getHeight();
-		int novaAltura = altura-50 ;
-		int novaLargura = largura +100;
+		int novaAltura = altura - 50;
+		int novaLargura = largura + 100;
 
 		BufferedImage novaImagem = new BufferedImage(novaAltura, novaLargura, BufferedImage.TYPE_BYTE_INDEXED);
 
 		// criar uma nova imagem com transparencia e com tamanho novo:
 		Graphics2D grafics = (Graphics2D) novaImagem.getGraphics();
-		grafics.drawImage(imagemOriginal, 0, 0, null); // para escrever uma imagem nova em cima da imagem antiga, usei o metodo drawImage 
+		grafics.drawImage(imagemOriginal, 0, 0, null); // para escrever uma imagem nova em cima da imagem antiga, usei o
+														// metodo drawImage
 
 		// copiar a imagem original para a nova imagem(em memoria)
 
-		
 		// Configurar Uma fonte:
-		Font fonte= new Font(Font.SANS_SERIF,Font.BOLD, 32 );
+		Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 32);
 		grafics.setColor(Color.YELLOW);
 		grafics.setFont(fonte);
 
 		// escrever uma frase na nova imagem
-		grafics.drawString("TOPT10", 0, novaLargura-20);
-		
+		grafics.drawString("TOPT10", 0, novaLargura - 20);
+
 		// escrever uma nova imagem em um arquivo
-		ImageIO.write(novaImagem, "jpg", new File("saida/sda.jpg"));
-		
+		ImageIO.write(novaImagem, "jpg", new File("saida/"+nomeArquivo));
+
 	}
-	
-	
-	
 	public static void main(String[] args) throws Exception {
 		
-		var gerador= new GeradorDeFigurinhas();
-		gerador.criar();
+		String texto= "texto.jpg";
+		String url = "https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg";
+		InputStream ipsUrl = new URL(url).openStream();
 		
-		System.out.println("saio??" );
+		new GeradorDeFigurinhas().criar(ipsUrl, texto);
+		System.out.println("ve ai se foi...");
 		
 	}
+
 }
